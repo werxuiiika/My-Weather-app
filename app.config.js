091d49f0a6e3036eb91617f9198b32fcaef6c1f9
bladvin <werxuiiika@gmail.com>
2026-08-23 06:31:28 +0300

    Bump version to 1.0.3 and use real app icon

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Моя погода",
     slug: "my-app-final",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
