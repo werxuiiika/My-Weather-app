2da595be088d854f8d25ee62cf92d7e59ae76f7c
bladvin <werxuiiika@gmail.com>
2026-08-23 15:34:23 +0300

    Bump version to 1.0.9

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Моя погода",
     slug: "my-app-final",
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
